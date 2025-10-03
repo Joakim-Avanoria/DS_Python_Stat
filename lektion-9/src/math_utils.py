5f0371ef45e54308a3abd3d5975ece8eae6a70c5
Joakim Lindh <joakim.i.lindh@gmail.com>
2025-10-03 19:07:36 +0200

    vecka 2

---
+++ lektion-9/src/math_utils.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a + b
+
+def multipy(a, b):
+    return a * b
+
+
+# print("Namnet på filen är:", __name__)
+
+if __name__ == "__main__":
+    print(add(3, 7))
+    print(multipy(2, 10))
